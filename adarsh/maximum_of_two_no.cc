346f5dd0433a9b5f78d50e24037d2c67c219f8ec
adarsh3149 <99113505+adarsh3149@users.noreply.github.com>
2022-02-12 16:35:58 +0530

    week1 assignments

---
+++ adarsh/maximum_of_two_no.cc
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int A,B;
+	printf("\n enter the values of A and B");
+	scanf("%d%d",&A,&B);
+	if(A>B)
+	printf("\n A is maximum");
+	else
+	printf("B is maximum");
+	return 0;
+}
+
